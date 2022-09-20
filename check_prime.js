eeb6fb6be1b3add518a608407a3305b7d727a678
Sushilroar <petersushil07@gmail.com>
2022-09-20 11:32:52 +0530

    code written

---
+++ check_prime.js
@@ -0,0 +1,22 @@
+function prime(number){
+   let factor=0;
+   for(let i=1;i<=number;i++}
+   if(number%i==0){
+        factor++;
+
+     }
+}
+	if(factor==2){
+	   return true;
+	}
+	 return false; 
+}
+	let answer=prime(13);
+
+	if(answer==true){
+	        console.log("prime");
+	}
+
+	else{
+	    console.log("not')
+}
